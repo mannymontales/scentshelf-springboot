143b0a4669b456bea79a47e8a13d821fd0c5e92c
Montales, Emmanuel R <Emmanuel.Montales@amtrak.com>
2023-09-16 10:27:06 -0400

    initial commit

---
+++ src/main/java/com/app/scentshelf/ScentshelfApplication.java
@@ -0,0 +1,13 @@
+package com.app.scentshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScentshelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/scentshelf/ScentshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.scentshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
